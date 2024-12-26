53ffa87bfaf421dcfa400454080a572aaa062069
oleksandr.aleksashyn <oleksandr.aleksashyn@gmail.com>
2024-12-26 17:48:48 +0200

    Add requested functionality to Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,24 @@
 package app;
 
+import java.util.function.Consumer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        var repository = new UserRepository();
+        Consumer<User> ifPresent = System.out::println;
+        Runnable notFound = () -> System.out.println("User was not found.");
+
+        //By id
+        repository.findUserById(1).ifPresentOrElse(ifPresent, notFound);
+        repository.findUserById(11).ifPresentOrElse(ifPresent, notFound);
+
+        //By email
+        repository.findUserByEmail("bfussell3@slate.com").ifPresentOrElse(ifPresent, notFound);
+        repository.findUserByEmail("test@test.com").ifPresentOrElse(ifPresent, notFound);
+
+        //Add users
+        repository.findAllUsers()
+                .ifPresentOrElse(users -> System.out.println("Found " + users.size() + " users"),
+                        () -> System.out.println("No users found."));
     }
 }
